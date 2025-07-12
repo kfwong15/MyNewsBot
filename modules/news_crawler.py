b38690a5447213319cb5c5886430cdc0a50a751f
kfwong15 <kfwong15@gmail.com>
2025-07-13 07:10:11 +0800

    Update news_crawler.py

--- modules/news_crawler.py
+++ modules/news_crawler.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 logger = logging.getLogger('news_crawler')
 
 # Google 新闻（中文）RSS，包含马来西亚新闻
-RSS_FEED = "https://news.google.com/rss/search?q=马来西亚&hl=zh-CN&gl=MY&ceid=MY:zh-Hans"
+RSS_FEED = "https://www.orientaldaily.com.my/rss/1"
 MIN_COUNT = 10
 
 HEADERS = {
